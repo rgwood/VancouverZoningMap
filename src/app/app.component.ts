1514ed19ed6cb4dc72556ffabc43ec18345c189c
Reilly <reilly.wood@icloud.com>
2018-01-06 15:33:22 -0800

    Leaflet basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as L from 'leaflet';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,8 @@ import * as L from 'leaflet';
 })
 export class AppComponent {
   //title = 'app';
+
+  constructor(private http: Http) { }
 
   ngOnInit(): void {
     console.log();
@@ -22,6 +25,24 @@ export class AppComponent {
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
       id: 'mapbox.streets'
     }).addTo(mymap);
+
+    this.http.get("assets/parcels.geojson")
+    .subscribe((data) => {
+      L.geoJSON(data.json()).addTo(mymap);
+    },
+    err => { console.log(err); });
+
+
+    // function onEachFeature(feature, layer) {
+    //   if (feature.properties) {
+    //     var area = areaCalculator.geometry(feature.geometry);
+    //     layer.bindPopup(`${feature.properties.ZONE_NAME}, ${feature.properties.CATEGORY}. Area: ${area.toFixed(0)} m`);
+    //   }
+    // }
+    //  getJSON('zoning.geojson',
+    //   function (data) {
+    //     L.geoJSON(data.features, { onEachFeature: onEachFeature }).addTo(mymap);
+    //   });
   
   }
 }
