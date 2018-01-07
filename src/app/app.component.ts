d12adfb1f50a37aba5525adb9ac99ddb0d777915
Reilly <reilly.wood@icloud.com>
2018-01-07 08:25:17 -0800

    replace leaflet with mapbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import * as L from 'leaflet';
 import { Http } from '@angular/http';
+import  {} from 'mapbox-gl';
 
 @Component({
   selector: 'app-root',
@@ -16,21 +16,35 @@ export class AppComponent {
     console.log();
     console.log(document);
 
-    var mymap: L.Map = L.map('map').setView([49.246292, -123.116226], 13);
+    mapboxgl.accessToken = "pk.eyJ1IjoiZ3JpZHN2YW5jb3V2ZXIiLCJhIjoiY2pjM3poNHBuMThqNTJ3cGZ2ZnZhbzd3OCJ9.-u65hk-BENoC1ZvnLEsH6Q";
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-      maxZoom: 18,
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      id: 'mapbox.streets'
-    }).addTo(mymap);
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [ -123.116226, 49.246292],
+      zoom: 13
+  });
 
-    this.http.get("assets/parcels.geojson")
-    .subscribe((data) => {
-      L.geoJSON(data.json()).addTo(mymap);
-    },
-    err => { console.log(err); });
+  map.on('load', () => {
+    console.log(map.getStyle().layers);
+  });
+
+    // var mymap: L.Map = L.map('map').setView([49.246292, -123.116226], 13);
+
+    // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    //   maxZoom: 18,
+    //   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    //     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    //     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    //   id: 'mapbox.streets'
+    // }).addTo(mymap);
+
+
+    // this.http.get("assets/parcels.geojson")
+    // .subscribe((data) => {
+    //   L.geoJSON(data.json()).addTo(mymap);
+    // },
+    // err => { console.log(err); });
 
 
     // function onEachFeature(feature, layer) {
