edfd9174c3e62506f2b322f2dc4a2236b1fbb8ce
Reilly <reilly.wood@icloud.com>
2018-01-07 11:30:43 -0800

    Query my tileserver

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,7 @@ import  {} from 'mapbox-gl';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //title = 'app';
-
+ 
   constructor(private http: Http) { }
 
   ngOnInit(): void {
@@ -25,9 +24,132 @@ export class AppComponent {
       zoom: 13
   });
 
-  map.on('load', () => {
-    console.log(map.getStyle().layers);
-  });
+    map.on('load', () => {
+      map.addSource("parcels-source", {
+        "type": "vector",
+        "tiles": ["http://localhost:7071/api/GetTile/{z}/{x}/{y}.mvt"]
+      });
+
+        map.addLayer({
+                       "id": "random",
+                       "type": "fill",
+                       "source": "parcels-source",
+                       "source-layer": "parcels-source",
+                       "paint": {
+                           "fill-color": "#3887be"
+                       }});
+    });
+
+
+  // map.on('load', () => {
+  //   map.addLayer({
+  //     "id": "terrain-data",
+  //     "type": "line",
+  //     "source": {
+  //         type: 'vector',
+  //         url: 'https://vector.mapzen.com/osm/all/{z}/{x}/{y}.mvt'
+  //         //url: 'http://localhost:7071/api/GetTile/{z}/{x}/{y}.mvt'
+  //     },
+  //     "source-layer": "contour",
+  //     "layout": {
+  //         "line-join": "round",
+  //         "line-cap": "round"
+  //     },
+  //     "paint": {
+  //         "line-color": "#ff69b4",
+  //         "line-width": 1
+  //     }
+  // });
+
+
+//   let simple: mapboxgl.Style =  {
+//     "version": 8,
+//     "sources": {
+//         "india_states": {
+//             "type": "vector",
+//             "tiles": ["http://localhost:7071/api/GetTile/{z}/{x}/{y}.mvt"]
+//         }
+//     },
+//     "layers": [{
+//             "id": "random",
+//             "type": "fill",
+//             "source": "india_states",
+//             "source-layer": "india_states",
+//             "paint": {
+//                 "fill-color": "#3887be"
+//             }
+//         },
+//         {
+//         "id": "random-line",
+//         "type": "line",
+//         "source": "india_states",
+//         "source-layer": "india_states",
+//         "layout": {
+//             "line-join": "round",
+//             "line-cap": "round"
+//         },
+//         "paint": {
+//             "line-color": "#ff69b4",
+//             "line-width": 1
+//         }
+//     }
+//     ]
+// };
+
+// var map = new mapboxgl.Map({
+//   container: 'map',
+//   style: simple,
+//   center: [ -123.116226, 49.246292],
+//   zoom: 13
+// });
+
+
+  //   var simple = {
+  //     "version": 8,
+  //     "sources": {
+  //         "osm": {
+  //             "type": "vector",
+  //             "scheme": "tms",
+  //             "tiles": ["http://geoserverHost:8080/geoserver/gwc/service/tms/1.0.0/opengeo:hessen@EPSG%3A900913@pbf/{z}/{x}/{y}.pbf"]
+  //             //"tiles": ["http://TegolaServerHost:8082/maps/zoning/{z}/{x}/{y}.vector.pbf"]
+  //         }
+  //     },
+  //     "layers": [
+  //         {
+  //             "id": "background",
+  //             "type": "background",
+  //             "paint": {
+  //                 "background-color": "#ffffff"
+  //             }
+  //         }, {
+  //             "id": "hessen",
+  //             "type": "line",
+  //             "source": "osm",
+  //             "source-layer": "hessen",
+  //             "filter": ["==", "$type", "LineString"],
+  //             "paint": {
+  //                 "line-color": "#3887be"
+  //             }
+  //         }
+  //     ]
+  // };
+  // map.addLayer(simple);
+
+    // map.addLayer({
+    //   'id': 'parcels',
+    //   'type': 'fill',
+    //   'source': {
+    //       'type': 'vector',
+    //       'url': 'http://localhost:7071/api/GetTile/{z}/{x}/{y}.mvt'
+    //   },
+    //   'layout': {},
+    //   'paint': {
+    //       'fill-color': '#f08',
+    //       'fill-opacity': 0.4
+    //   }});
+
+ // });
+
 
     // var mymap: L.Map = L.map('map').setView([49.246292, -123.116226], 13);
 
