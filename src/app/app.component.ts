c8ed2eac15fe8f73ef55024440da6680ffa88441
Reilly <reilly.wood@icloud.com>
2018-01-13 16:45:33 -0800

    Use mbtiles server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
-import  {} from 'mapbox-gl';
+import { } from 'mapbox-gl';
 import { LngLat } from 'mapbox-gl/dist/mapbox-gl';
 
 @Component({
@@ -9,7 +9,7 @@ import { LngLat } from 'mapbox-gl/dist/mapbox-gl';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
+
   constructor(private http: Http) { }
 
   ngOnInit(): void {
@@ -21,180 +21,81 @@ export class AppComponent {
     var map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v9',
-      center: [ -123.116226, 49.246292],
+      center: [-123.116226, 49.246292],
       zoom: 11
-  });
+    });
 
     map.on('load', () => {
       map.addSource("parcelsSource", {
         "type": "vector",
-        "tiles": ["http://localhost:3000/mvt/{x}/{y}/{z}"],
-        "minzoom":11,
-        "maxzoom":11
+        "tiles": ["http://localhost:3000/parcels/{z}/{x}/{y}.pbf"],
+        "minzoom": 11,
+        "maxzoom": 15
       });
 
-        map.addLayer({
-                       "id": "parcelLayer",
-                       "type": "fill",
-                       "source": "parcelsSource",
-                       "source-layer": "default",
-                       "paint": {
-                           "fill-color": [
-                            "rgb",
-                            // red is higher when feature.properties.random is higher
-                            ["get", "random"],
-                            0,
-                            50
-                        ]
-                       }});
-        map.addControl(new mapboxgl.NavigationControl());
+      let layers: mapboxgl.Layer[] = map.getStyle().layers;
+      // Find the index of the first symbol layer in the map style
+      let firstSymbolId: string;
+      for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol') {
+          firstSymbolId = layers[i].id;
+          break;
+        }
+      }
+
+      map.addLayer({
+        "id": "parcelLayer",
+        "type": "fill",
+        "source": "parcelsSource",
+        "source-layer": "default",
+        "paint": {
+          "fill-color": [
+            "rgb",
+            //["get", "random"],
+            30,
+            0,
+            50
+          ]
+        }
+      }, firstSymbolId);
+      map.addControl(new mapboxgl.NavigationControl());
     });
 
     map.on('click', 'parcelLayer', function (e) {
       new mapboxgl.Popup()
-          .setLngLat(e.lngLat)
-          .setHTML(e.features[0].properties.streetname)
-          .addTo(map);
+        .setLngLat(e.lngLat)
+        .setHTML(`${e.features[0].properties.civic_no} ${e.features[0].properties.streetname}`)
+        .addTo(map);
+    });
+
+    
+    map.on('mousemove', function (e) {
+      var features = map.queryRenderedFeatures(e.point,{layers:["parcelLayer"]});
+      map.getCanvas().style.cursor = features.length ? 'pointer' : '';
   });
 
-  // map.on('load', () => {
-  //   map.addLayer({
-  //     "id": "terrain-data",
-  //     "type": "line",
-  //     "source": {
-  //         type: 'vector',
-  //         url: 'https://vector.mapzen.com/osm/all/{z}/{x}/{y}.mvt'
-  //         //url: 'http://localhost:7071/api/GetTile/{z}/{x}/{y}.mvt'
-  //     },
-  //     "source-layer": "contour",
-  //     "layout": {
-  //         "line-join": "round",
-  //         "line-cap": "round"
-  //     },
-  //     "paint": {
-  //         "line-color": "#ff69b4",
-  //         "line-width": 1
-  //     }
-  // });
+    // map.on('load', () => {
+    //   map.addLayer({
+    //     "id": "terrain-data",
+    //     "type": "line",
+    //     "source": {
+    //         type: 'vector',
+    //         url: 'https://vector.mapzen.com/osm/all/{z}/{x}/{y}.mvt'
+    //         //url: 'http://localhost:7071/api/GetTile/{z}/{x}/{y}.mvt'
+    //     },
+    //     "source-layer": "contour",
+    //     "layout": {
+    //         "line-join": "round",
+    //         "line-cap": "round"
+    //     },
+    //     "paint": {
+    //         "line-color": "#ff69b4",
+    //         "line-width": 1
+    //     }
+    // });
 
 
-//   let simple: mapboxgl.Style =  {
-//     "version": 8,
-//     "sources": {
-//         "india_states": {
-//             "type": "vector",
-//             "tiles": ["http://localhost:7071/api/GetTile/{z}/{x}/{y}.mvt"]
-//         }
-//     },
-//     "layers": [{
-//             "id": "random",
-//             "type": "fill",
-//             "source": "india_states",
-//             "source-layer": "india_states",
-//             "paint": {
-//                 "fill-color": "#3887be"
-//             }
-//         },
-//         {
-//         "id": "random-line",
-//         "type": "line",
-//         "source": "india_states",
-//         "source-layer": "india_states",
-//         "layout": {
-//             "line-join": "round",
-//             "line-cap": "round"
-//         },
-//         "paint": {
-//             "line-color": "#ff69b4",
-//             "line-width": 1
-//         }
-//     }
-//     ]
-// };
-
-// var map = new mapboxgl.Map({
-//   container: 'map',
-//   style: simple,
-//   center: [ -123.116226, 49.246292],
-//   zoom: 13
-// });
 
 
-  //   var simple = {
-  //     "version": 8,
-  //     "sources": {
-  //         "osm": {
-  //             "type": "vector",
-  //             "scheme": "tms",
-  //             "tiles": ["http://geoserverHost:8080/geoserver/gwc/service/tms/1.0.0/opengeo:hessen@EPSG%3A900913@pbf/{z}/{x}/{y}.pbf"]
-  //             //"tiles": ["http://TegolaServerHost:8082/maps/zoning/{z}/{x}/{y}.vector.pbf"]
-  //         }
-  //     },
-  //     "layers": [
-  //         {
-  //             "id": "background",
-  //             "type": "background",
-  //             "paint": {
-  //                 "background-color": "#ffffff"
-  //             }
-  //         }, {
-  //             "id": "hessen",
-  //             "type": "line",
-  //             "source": "osm",
-  //             "source-layer": "hessen",
-  //             "filter": ["==", "$type", "LineString"],
-  //             "paint": {
-  //                 "line-color": "#3887be"
-  //             }
-  //         }
-  //     ]
-  // };
-  // map.addLayer(simple);
-
-    // map.addLayer({
-    //   'id': 'parcels',
-    //   'type': 'fill',
-    //   'source': {
-    //       'type': 'vector',
-    //       'url': 'http://localhost:7071/api/GetTile/{z}/{x}/{y}.mvt'
-    //   },
-    //   'layout': {},
-    //   'paint': {
-    //       'fill-color': '#f08',
-    //       'fill-opacity': 0.4
-    //   }});
-
- // });
-
-
-    // var mymap: L.Map = L.map('map').setView([49.246292, -123.116226], 13);
-
-    // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    //   maxZoom: 18,
-    //   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-    //     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    //     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    //   id: 'mapbox.streets'
-    // }).addTo(mymap);
-
-
-    // this.http.get("assets/parcels.geojson")
-    // .subscribe((data) => {
-    //   L.geoJSON(data.json()).addTo(mymap);
-    // },
-    // err => { console.log(err); });
-
-
-    // function onEachFeature(feature, layer) {
-    //   if (feature.properties) {
-    //     var area = areaCalculator.geometry(feature.geometry);
-    //     layer.bindPopup(`${feature.properties.ZONE_NAME}, ${feature.properties.CATEGORY}. Area: ${area.toFixed(0)} m`);
-    //   }
-    // }
-    //  getJSON('zoning.geojson',
-    //   function (data) {
-    //     L.geoJSON(data.features, { onEachFeature: onEachFeature }).addTo(mymap);
-    //   });
-  
   }
 }
