4de6d428120f7469ab0d0cf4bdf52d85c68539b9
Reilly <reilly.wood@icloud.com>
2018-01-23 18:26:34 -0800

    initial toggle and legend work, 2 colours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { } from 'mapbox-gl';
 import { LngLat } from 'mapbox-gl/dist/mapbox-gl';
-
+import { ToggleControl } from 'mapbox-gl-toggle-control';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  legendDisplayStyle: string = "none"; 
 
   constructor(private http: Http) { }
 
@@ -20,20 +22,24 @@ export class AppComponent {
 
     var map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v9',
+      style: 'mapbox://styles/mapbox/dark-v9',
       center: [-123.116226, 49.246292],
       zoom: 12
     });
 
     map.addControl(new mapboxgl.GeolocateControl({
       positionOptions: {
-          enableHighAccuracy: true
+          enableHighAccuracy: true,
+          timeout:10000 //in ms
       },
       trackUserLocation: true
   }));
     map.addControl(new mapboxgl.NavigationControl());
-
     map.on('load', () => {
+
+      //todo: fix. Currently not working because querySelector can't get the #legend element
+      //var toggle = new ToggleControl(document.querySelector('#legend'))
+      //map.addControl(toggle, 'top-left')
 
       let layers: mapboxgl.Layer[] = map.getStyle().layers;
       // Find the index of the first symbol layer in the map style
@@ -58,9 +64,16 @@ export class AppComponent {
           "source": "parcelsSource",
           "source-layer": "default",
           "paint": {
-            "fill-color": [
+            "fill-opacity": 0.8,
+            "fill-color": 
+            ["case", 
+              //yellow for SFHs
+              ["get", "sfh_only"],["rgb", 255,255,0],
+              //default blue
+              ["rgb", 0,102,255]
+          ]
+            /*[
               "rgb",
-
               70,
               70,
               //blue is 255 for buildings built in 2018, less for older buildings
@@ -69,23 +82,24 @@ export class AppComponent {
                   ["-", 2018, ["get", "year_built"]], //age
                 ]
               ]
-            ]
+            ]*/
           }
         }, firstSymbolId)
         .on('click', 'parcelLayer', function (e) {
+          console.log(e.features);
           new mapboxgl.Popup()
             .setLngLat(e.lngLat)
             .setHTML(`${e.features[0].properties.address}<br>
                       ${e.features[0].properties.area_sq_m.toLocaleString('en-us')} m<sup>2</sup><br>
                       Built in ${e.features[0].properties.year_built}.<br>
-                      Zoning: ${e.features[0].properties.zone_name}`)
+                      Zoning: ${e.features[0].properties.zone_name}<br>
+                      SFH only: ${e.features[0].properties.sfh_only}`)
             .addTo(map);
         })
         .on('mousemove', function (e) {
           var features = map.queryRenderedFeatures(e.point, { layers: ["parcelLayer"] });
           map.getCanvas().style.cursor = features.length ? 'pointer' : '';
         });
-
     });
 
     // map.on('load', () => {
@@ -107,9 +121,5 @@ export class AppComponent {
     //         "line-width": 1
     //     }
     // });
-
-
-
-
   }
 }
