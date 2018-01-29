0afa701398eefafc9635f6e06575d537e2f5846c
Reilly <reilly.wood@icloud.com>
2018-01-28 23:15:58 -0800

    Formatting tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { } from 'mapbox-gl';
 import { LngLat, Control } from 'mapbox-gl/dist/mapbox-gl';
-import {CustomMapboxControl} from './CustomMapboxControl';
+import { CustomMapboxControl } from './CustomMapboxControl';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -25,14 +25,14 @@ export class AppComponent {
 
     map.addControl(new mapboxgl.GeolocateControl({
       positionOptions: {
-          enableHighAccuracy: true,
-          timeout:10000 //in ms
+        enableHighAccuracy: true,
+        timeout: 10000 //in ms
       },
       trackUserLocation: true
-  }));
-  //todo: hide navigation control on mobile because two-finger zoom/rotate is better
+    }));
+    //todo: hide navigation control on mobile because two-finger zoom/rotate is better
     map.addControl(new mapboxgl.NavigationControl());
-    var customControl = new CustomMapboxControl( () => this.toggleLegendVisibility());
+    var customControl = new CustomMapboxControl(() => this.toggleLegendVisibility());
     map.addControl(customControl);
 
     map.on('load', () => {
@@ -62,36 +62,27 @@ export class AppComponent {
           "source-layer": "default",
           "paint": {
             "fill-opacity": 0.8,
-            "fill-color": 
-            ["case", 
-              //yellow for SFHs
-              ["get", "sfh_only"],["rgb", 255,255,0],
-              //default blue
-              ["rgb", 0,102,255]
-          ]
-            /*[
-              "rgb",
-              70,
-              70,
-              //blue is 255 for buildings built in 2018, less for older buildings
-              ["-", 255,
-                ["*", 2,
-                  ["-", 2018, ["get", "year_built"]], //age
-                ]
+            "fill-color":
+              ["case",
+                //yellow for SFHs
+                ["get", "sfh_only"], ["rgb", 255, 255, 0],
+                //default blue
+                ["rgb", 0, 102, 255]
               ]
-            ]*/
           }
         }, firstSymbolId)
         .on('click', 'parcelLayer', function (e) {
           let areaInSqMetres = Number(e.features[0].properties.area_sq_m);
-          let areaInSqFt: number =Math.round(areaInSqMetres * 10.76391); //square feet in 1 sq m
+          let areaInSqFt: number = Math.round(areaInSqMetres * 10.76391); //square feet in 1 sq m
           new mapboxgl.Popup()
             .setLngLat(e.lngLat)
+            //I do not like that I'm building HTML manually here, but prerendering an Angular component seems fairly unpleasant these days.
+            //Revisit this if the popup gets more complex
             .setHTML(`<strong>${e.features[0].properties.address}</strong><br>
                       Zoning: ${e.features[0].properties.zone_name || 'Unknown'} (<a href='TODO' target='_blank'>details</a>)<br>
                       Area: ${areaInSqMetres.toLocaleString('en-us')} m<sup>2</sup> 
                            (${areaInSqFt.toLocaleString('en-us')} ft<sup>2</sup>)<br>
-                      Built in: ${ e.features[0].properties.year_built || 'N/A' }`)
+                      Built in: ${ e.features[0].properties.year_built || 'N/A'}`)
             .addTo(map);
         })
         .on('mousemove', function (e) {
@@ -102,10 +93,10 @@ export class AppComponent {
   }
 
   toggleLegendVisibility(): void {
-    if(this.legendDisplayStyle == ''){
+    if (this.legendDisplayStyle == '') {
       this.legendDisplayStyle = 'none'
     }
-    else{
+    else {
       this.legendDisplayStyle = '';
     }
   }
